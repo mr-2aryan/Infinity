819f7f56985eb04afb9c6c970f8e4f416dcefadc
Aryan Shahid <aryanshahid098@gmail.com>
2025-12-15 16:04:20 +0500

    Latest

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import Home from './Pages/Home'
-import Shop from './Pages/Shop'
-import About from './Pages/About'
-import View from './Pages/View'
-import Contact from './Pages/Contact'
-import Checkout from './Pages/Checkout'
-import Confirmation from './Pages/Confirmation'
 import Layout from './components/JSX/Layout'
+import LoadingOverlay from './components/JSX/LoadingOverlay';
+
+// Lazy loading components
+const Home = React.lazy(() => import('./Pages/Home'));
+const Shop = React.lazy(() => import('./Pages/Shop'));
+const About = React.lazy(() => import('./Pages/About'));
+const View = React.lazy(() => import('./Pages/View'));
+const Contact = React.lazy(() => import('./Pages/Contact'));
+const Checkout = React.lazy(() => import('./Pages/Checkout'));
+const Confirmation = React.lazy(() => import('./Pages/Confirmation'));
 
 function App() {
 
@@ -21,17 +24,19 @@ function App() {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-        <Route path="/view/:id" element={<View />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<LoadingOverlay />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/view/:id" element={<View />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
